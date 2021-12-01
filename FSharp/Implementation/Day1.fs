b53421805d7e97b1dd7a77f63dc5e71161fd2696
Kai Jellinghaus <contact@kaij.tech>
2021-12-01 06:46:41 +0100

    Day1Part1 Solution in FSharp

---
+++ FSharp/Implementation/Day1.fs
@@ -0,0 +1,14 @@
+﻿namespace Implementation
+
+open System
+
+type Day1 =
+    
+    static member Part1 (input : string) =
+        let notNullOrWhiteSpace p = not (String.IsNullOrWhiteSpace p) 
+        input.Split '\n'
+        |> Seq.ofArray
+        |> Seq.filter notNullOrWhiteSpace
+        |> Seq.pairwise
+        |> Seq.filter (fun (a, b) -> a < b)
+        |> Seq.length
--- FSharp/Tests/Program.fs
+++ FSharp/Tests/Program.fs
@@ -1,1 +1,19 @@
-module Program = let [<EntryPoint>] main _ = 0
+module Program
+
+open Implementation
+open Xunit
+
+let [<Fact>] Day1Part1() = Assert.Equal (7, Day1.Part1 @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263")
+
+
+
+let [<EntryPoint>] main _ = 0
