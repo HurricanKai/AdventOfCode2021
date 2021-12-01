72184fa50849e8d99d60311bca76d7583eafd19b
Kai Jellinghaus <contact@kaij.tech>
2021-12-01 07:08:31 +0100

    Day1Part2 Solution in FSharp

--- FSharp/Implementation/Day1.fs
+++ FSharp/Implementation/Day1.fs
@@ -9,6 +9,20 @@ type Day1 =
         input.Split '\n'
         |> Seq.ofArray
         |> Seq.filter notNullOrWhiteSpace
+        |> Seq.map Int32.Parse
+        |> Seq.pairwise
+        |> Seq.filter (fun (a, b) -> a < b)
+        |> Seq.length
+    
+    static member Part2 (input : string) =
+        let notNullOrWhiteSpace p = not (String.IsNullOrWhiteSpace p) 
+        input.Split '\n'
+        |> Seq.ofArray
+        |> Seq.filter notNullOrWhiteSpace
+        |> Seq.map Int32.Parse
+        |> Seq.map float
+        |> Seq.windowed 3
+        |> Seq.map Array.average
         |> Seq.pairwise
         |> Seq.filter (fun (a, b) -> a < b)
         |> Seq.length
--- FSharp/Tests/Program.fs
+++ FSharp/Tests/Program.fs
@@ -14,6 +14,17 @@ let [<Fact>] Day1Part1() = Assert.Equal (7, Day1.Part1 @"199
 260
 263")
 
+let [<Fact>] Day1Part2() = Assert.Equal (5, Day1.Part2 @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263")
+
 
 
 let [<EntryPoint>] main _ = 0
